73d7b34b483cb713407aaf5b38923f8907587b2d
Bruna Veiga Chalegre de Lira <brunaveigachalegre2@gmail.com>
2025-03-13 10:59:48 -0300

    a

---
+++ PrimeirosPassinhosApp/PrimeirosPassinhosApp/PrimeirosPassinhosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeirosPassinhosAppApp.swift
+//  PrimeirosPassinhosApp
+//
+//  Created by Bruna Veiga Chalegre de Lira on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PrimeirosPassinhosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
